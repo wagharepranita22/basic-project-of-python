8c22d7e6d17cb174d552a4be809fd14e3afd809c
wagharepranita22 <78723120+wagharepranita22@users.noreply.github.com>
2024-08-28 01:09:12 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,4 @@
+print("Welcome to the know slip method")
+string = input("Give your name: ")
+a = string.split(" ")
+print(a)
---
+++ virtual toss.py
@@ -0,0 +1,6 @@
+import random
+choice =random.randint(0,1)
+if choice == 0:
+    print("Heads")
+else:
+    print("Tails")
